10f1adf96e27e5c7a320a87d3d375f973ecec4c0
chandan-ray-522 <contact@stockan.in>
2026-05-04 09:54:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ TOKEN = os.environ['SECRET_TOKEN']
 # --- CONFIGURATION ---
 # 'DAILY' = Rozana automation (7:30 PM trigger)
 # 'HISTORICAL' = Purana data (12 din mein 12 mahine)
-MODE = 'HISTORICAL' 
+MODE = 'DAILY' 
 
 # Agar MODE 'HISTORICAL' hai, toh yahan Dates badlein (YYYY-MM-DD)
 START_DATE = "2026-04-01"
